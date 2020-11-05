2dc945933b9c882cf4d97bfd8046a3cebd2eecd4
KDY <doyoung0205@naver.com>
2020-11-05 15:41:56 +0900

    first commit

---
+++ dist/precache-manifest.85a8798cc13964ca3888a09b98c2fd90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8dd53147b27a350849",
+    "url": "/css/app.3c83c013.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2a1a0a82228c180d58950bec42ef98db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "439aa22c24585ef52f21",
+    "url": "/js/about.be3a22b3.js"
+  },
+  {
+    "revision": "4d8dd53147b27a350849",
+    "url": "/js/app.6f8e46c4.js"
+  },
+  {
+    "revision": "cb82d6dee2ff633ad033",
+    "url": "/js/chunk-vendors.12f5b8e8.js"
+  },
+  {
+    "revision": "99f7aedda83dea048a612ae2e7e51648",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
